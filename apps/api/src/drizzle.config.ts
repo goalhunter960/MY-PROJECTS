d1c86ec91d5151a0b5de840f22371ce74d268cdf
goalhunter960 <goalhunter960@gmail.com>
2025-06-25 16:53:59 -0400

    Fix drizzle config for PostgreSQL

--- apps/api/src/drizzle.config.ts
+++ apps/api/src/drizzle.config.ts
@@ -1,11 +1,13 @@
-import 'dotenv/config'
 import type { Config } from 'drizzle-kit'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 export default {
   schema: './src/schema.ts',
   out: './drizzle',
   driver: 'pg',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL as string,
+    connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config
+
